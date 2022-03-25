4c6bbe930de288924795fea9d9e50592271cdf6d
amirabroun <abroon234@gmail.com>
2022-03-25 22:45:14 +0430

    feature/add new router composer package, fix/route structure

--- index.php
+++ index.php
@@ -1,14 +1,2 @@
 <?php require __DIR__ . "/vendor/autoload.php";
-
-use App\Config\Config;
-use Route\Router\Route;
-
-Route::get('/test', '/Resource/test.php');
-Route::get('/', '/Resource/index.php');
-
-Route::get('/login/secret/' . md5(Config::SECRET_LOGIN), '/Resource/login.php');
-
-Route::get('/products' , '/Resource/products.php');
-Route::get('/brands' , '/Resource/brands.php');
-Route::get('/categories' , '/Resource/categories.php');
-Route::get('/users' , '/Resource/users.php');
+require __DIR__ . "/Route/route.php"; 
