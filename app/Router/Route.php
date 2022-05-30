7d38cb6b205929994704618d87a153cecd193d19
Amir Abroun <abroon234@gmail.com>
2022-05-30 18:45:42 +0430

    refactor: Route has a new get function

--- app/Router/Route.php
+++ app/Router/Route.php
@@ -6,11 +6,11 @@ class Route
 {
     private static $resources = '/resources/Views/';
 
-    public static function newGet($route, $path)
+    public static function get(string $uri, $path)
     {
         if ($_SERVER['REQUEST_METHOD'] !== 'GET') return;
 
-        $routeSite = explode('/', $route);
+        $routeSite = explode('/', trim($uri, '/'));
         $routeUri = explode('/', uri());
 
         if (count($routeSite) !== count($routeUri)) return;
@@ -22,88 +22,18 @@ class Route
             $var = trim($partRoute, '{}');
             $$var = ($routeUri[$key]);
         }
-        dd($name, $phoneNumber); // users/{name}/mobile/{phoneNumber}
-        // dd($var, $$var);
-    }
-
-    public static function get($route, $path)
-    {
-        if ($_SERVER['REQUEST_METHOD'] !== 'GET') return;
 
         strpos($path, 'resources')
-            ? self::route($route, $path)
-            : self::route($route, self::$resources . preparePath($path));
+            ? include_once($_SERVER['DOCUMENT_ROOT'] . $path)
+            : include_once($_SERVER['DOCUMENT_ROOT'] . self::$resources . preparePath($path));
     }
 
     public static function post(string $uri, array|string|null $action = null)
     {
-        return checkPostUri($uri) ? new Router($uri, $action) : new Router;
+        return checkPostUri($uri)
+            ? new Router($uri, $action)
+            : new Router;
     }
 
-    public static function put($route, $path)
-    {
-        if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
-            self::route($route, $path);
-        }
-    }
-
-    public static function patch($route, $path)
-    {
-        if ($_SERVER['REQUEST_METHOD'] == 'PATCH') {
-            self::route($route, $path);
-        }
-    }
-
-    public static function delete($route, $path)
-    {
-        if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
-            self::route($route, $path);
-        }
-    }
-
-    public static function any($route, $path)
-    {
-        self::route($route, $path);
-    }
-
-    private static function route($route, $path)
-    {
-        $ROOT = $_SERVER['DOCUMENT_ROOT'];
-
-        if ($route == "/404") {
-            include_once("$ROOT/$path");
-            exit();
-        }
-
-        $route_parts = explode('/', $route);
-        $request_url_parts = explode('/', strtok(rtrim(filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL), '/'), '?'));
-        array_shift($route_parts);
-        array_shift($request_url_parts);
-
-        if ($route_parts[0] == '' && count($request_url_parts) == 0) {
-            include_once("$ROOT/$path");
-            exit();
-        }
-
-        if (count($route_parts) != count($request_url_parts)) {
-            return;
-        }
-
-        $parameters = [];
-
-        for ($__i__ = 0; $__i__ < count($route_parts); $__i__++) {
-            $route_part = $route_parts[$__i__];
-
-            if (preg_match("/^[$]/", $route_part)) {
-                $route_part = ltrim($route_part, '$');
-                array_push($parameters, $request_url_parts[$__i__]);
-                $$route_part = $request_url_parts[$__i__];
-            } else if ($route_parts[$__i__] != $request_url_parts[$__i__]) {
-                return;
-            }
-        }
-
-        include_once("$ROOT/$path");
-        exit();
-    }
+    // create: put, patch, delete, any
 }
--- routes/route.php
+++ routes/route.php
@@ -5,23 +5,23 @@ use App\Router\Route;
 Route::get('/test', 'test');
 Route::get('/', 'index');
 
-Route::get('/test/$name', 'test');
+Route::get('/test/{name}', 'test');
 
-Route::newGet('users/{name}/mobile/{phoneNumber}', '');
+Route::get('users/{name}/mobile/{phoneNumber}', 'test');
+Route::get('users/{name}', 'test');
 
 // login
 Route::get('/login/secret/' . md5(secretKey('secret_login')), 'auth.login');
-Route::post('/login/secret/' . md5(secretKey('secret_login')), 'LoginController@adminLogin');
-// Route::post('/login/secret/' . md5(secretKey('secret_login')))
-//     ->controller(App\Controllers\LoginController::class)
-//     ->function('adminLogin')
-//     ->name('name_action'); // set action
+// Route::post('/login/secret/' . md5(secretKey('secret_login')), 'LoginController@adminLogin');
+Route::post('/login/secret/' . md5(secretKey('secret_login')))
+    ->controller(App\Controllers\LoginController::class)
+    ->function('adminLogin');
 
 Route::post('log-out', 'LoginController@logOut');
 
 // product
 Route::get('/products', 'product.products');
-Route::get('/products/$id', 'product.single-product');
+Route::get('/products/{id}', 'product.single-product');
 
 Route::get('/brands', 'brand.brands');
 Route::get('/categories', 'category.categories');
